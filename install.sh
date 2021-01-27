3013459f09b53e30cece796371c4d2058ff3bb6e
thegahmer <thegahmer@hotmail.com>
2021-01-27 23:17:28 +0000

    second

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 mkdir /home/pi/nfc
-touch /home/pi/nfc/main.py
 cd /home/pi/nfc
-sudo apt-get install alsa-utils libnfc-dev 
-sed -i '1s/^/exec \/home\/pi\/nfc\/main &\n/' /etc/rc.local
+sudo apt-get install alsa-utils libnfc-dev i2c-tools
+sudo i2cdetect -y 1
+sudo sed -i '1s/^/exec \/home\/pi\/nfc\/main &\n/' /etc/rc.local
 curl https://srv-store1.gofile.io/download/R8byzT/off.wav > off.wav
 curl https://srv-store1.gofile.io/download/pZp4Ui/on.wav > on.wav
 curl https://pastebin.com/raw/nD6x40az > /home/pi/nfc/main.c
 gcc -o main main.c -lnfc 
-reboot
+zenity --info --text="The configuration menu will come up next. Make sure you enable i2c under advanced options!" --width 200 --height 100 --ok-label="I Understand"
+sudo raspi-config
